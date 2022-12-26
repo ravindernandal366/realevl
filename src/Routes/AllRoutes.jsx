a9cd68d1828282f14d26833ec58f56b139e91c36
RAVINDER NANDAL <ravindernandal366@gmail.com>
2022-12-26 15:33:29 +0000

    Update 3 files

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,7 +1,14 @@
+import { BrowserProvider } from "react-router-dom";
+import DashBoard from "./Dashboard";
+import Home from "./Home";
+import Login from "./Login";
+import SingleProductPage from "./SingleProductPage";
+
 function AllRoutes() {
   return (
     <div>
-
+      
+      dsddsd
     </div>
   );
 }
---
+++ src/Routes/nav.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+function Nav(){
+    
+    return <>
+        <Link to="/DashBoard"> DashBoard </Link>
+        <Link to="/Login"> Login </Link>
+        <Link to="/DashBoard"> DashBoard </Link>
+    </>
+}
+
+export default Nav;
